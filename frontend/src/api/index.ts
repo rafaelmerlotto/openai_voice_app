93e4001fe8a2c45c652c17849c9d9c003bdf9901
root <root@Rafael-Merlotto>
2024-02-28 12:44:18 +0100

    some fixes deploy

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,5 +1,5 @@
 import { ApiService } from "./api";
 
 
-const url:string = "https://openai-voice-app.onrender.com/";
+const url:string = "https://openai-voice-app.onrender.com";
 export const apiService:ApiService = new ApiService(`${url}`)
